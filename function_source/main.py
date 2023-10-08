81f591e2f72eb624755bda3f978dac894a1421d8
kaY-hR <harunorikawai@gmail.com>
2023-10-08 21:08:32 +0900

    不具合の修正

--- function_source/main.py
+++ function_source/main.py
@@ -3,5 +3,5 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index(requests):
     return render_template("index.html",text = "test!!")
--- main.py
+++
@@ -1,7 +0,0 @@
-from function_source.server import app
-
-def main():
-    app.run(debug=False, host='0.0.0.0', port=80)
-
-if __name__ == "__main__":
-    main()
