bcd3ce8e8b7b756801d3e36cf1e8c7cccc44b820
kaY-hR <harunorikawai@gmail.com>
2023-10-09 20:49:45 +0900

    ローカル実行対応

--- function_source/main.py
+++ function_source/main.py
@@ -3,5 +3,5 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index(requests):
-    return render_template("index.html",text = "test!!!!")
+def index(requests = None):
+    return render_template("index.html",text = "test")
