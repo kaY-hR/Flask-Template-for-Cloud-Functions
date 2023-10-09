68347f449ec9d8d154906ad4f9db77edf745b6b4
kaY-hR <harunorikawai@gmail.com>
2023-10-09 17:28:38 +0900

    cd テスト

--- function_source/main.py
+++ function_source/main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index(requests):
-    return render_template("index.html",text = "test?!")
+    return render_template("index.html",text = "test!!!!")
