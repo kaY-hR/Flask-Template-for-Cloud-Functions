398f02d2f7b410d02a20c419e0e105a673074229
kaY-hR <harunorikawai@gmail.com>
2023-10-11 16:07:48 +0900

    add a sample link

--- function_source/main.py
+++ function_source/main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index(requests = None):
     return render_template("index.html",text = "test")
+
+@app.route("/page1", methods=["GET", "POST"])
+def page1(requests = None):
+    return render_template("page1.html",text = "test")
