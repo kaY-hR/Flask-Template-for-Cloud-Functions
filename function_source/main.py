afdc11798713c0d99bf12b56e4503c02088db238
kaY-hR <harunorikawai@gmail.com>
2023-10-11 23:37:11 +0900

    マルチエンドポイントに対応 - 修正

--- function_source/main.py
+++ function_source/main.py
@@ -1,14 +1,18 @@
-from flask import render_template, Flask
-from .tools import entrypoint
-
+from flask import render_template, Flask, url_for, redirect
+from flask import request
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index(requests = None):
+    print(request.args.get('test'))
     return render_template("index.html",text = "test")
 
 @app.route("/page1", methods=["GET", "POST"])
 def page1(requests = None):
     return render_template("page1.html")
 
-app_wrap = lambda request: entrypoint(app, request)
+def entrypoint(requests):
+    func_name = request.args.get('func')
+    if not(func_name):
+        return index(requests)
+    return page1(requests)
